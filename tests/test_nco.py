bfeb2b826dc56aed5b8c9abe404c5c61a0bd916a
Tim Heap <tim.heap@csiro.au>
2024-06-26 14:06:49 +1000

    Properly isolate the nco tests

--- tests/test_nco.py
+++ tests/test_nco.py
@@ -1,5 +1,5 @@
 import subprocess
-from pathlib import Path
+import pathlib
 from unittest import mock
 
 import pytest
@@ -7,19 +7,27 @@ import pytest
 from emsarray import nco
 
 
-def test_ncrcat(monkeypatch: pytest.MonkeyPatch) -> None:
+def test_ncrcat(
+    tmp_path: pathlib.Path,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    out_file = tmp_path / 'out.nc'
+
     check_call = mock.MagicMock(subprocess.check_call)
     monkeypatch.setattr(subprocess, 'check_call', check_call)
 
-    nco.ncrcat(["one.nc", "two.nc"], "out.nc")
+    nco.ncrcat(["one.nc", "two.nc"], out_file)
     check_call.assert_called_once_with(
-        ['ncrcat', '-hH', '-o', 'out.nc', 'one.nc', 'two.nc'],
+        ['ncrcat', '-hH', '-o', str(out_file), 'one.nc', 'two.nc'],
         stdin=subprocess.DEVNULL,
     )
 
 
-def test_ncrcat_output_exists(tmpdir: Path, monkeypatch: pytest.MonkeyPatch) -> None:
-    out_file = Path(tmpdir) / 'out.nc'
+def test_ncrcat_output_exists(
+    tmp_path: pathlib.Path,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    out_file = tmp_path / 'out.nc'
     out_file.touch()
 
     check_call = mock.MagicMock(subprocess.check_call)
@@ -31,11 +39,17 @@ def test_ncrcat_output_exists(tmpdir: Path, monkeypatch: pytest.MonkeyPatch) -> 
     check_call.assert_not_called()
 
 
-def test_ncrcat_no_input_files(monkeypatch: pytest.MonkeyPatch) -> None:
+def test_ncrcat_no_input_files(
+    tmp_path: pathlib.Path,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    out_file = tmp_path / 'out.nc'
+
     check_call = mock.MagicMock(subprocess.check_call)
     monkeypatch.setattr(subprocess, 'check_call', check_call)
 
     with pytest.raises(ValueError):
-        nco.ncrcat([], "out.nc")
+        nco.ncrcat([], out_file)
 
     check_call.assert_not_called()
+    assert not out_file.exists()
