5b0c51bcbf9786406ef4f79bd1f38a4124405c66
David Secretan-Hallett <david.secretan-hallett@csiro.au>
2024-09-12 10:15:44 +1000

    Updated cache function docstrings.

--- src/emsarray/operations/cache.py
+++ src/emsarray/operations/cache.py
@@ -20,8 +20,8 @@ def hash_attributes(hash: hashlib._Hash, attribute_dict: xarray.Variable) -> Non
     hash : hashlib-style hash instance
         The hash instance to update with geometry data.
         This must follow the interface defined in :mod:`hashlib`.
-    attribute_dict: python dictionary instance
-        Only hashable dict values will be encoded, the rest will be ignored.
+    attribute_dict: xarray.Variable dictionary
+        Expects a pickleable xarray.Variable dictionary
     """
     # Specifying pickle protocol version to prevent hash changes when the default is updated
     attribute_dict_bytes = pickle.dumps(attribute_dict, protocol=5)
@@ -38,7 +38,7 @@ def make_cache_key(dataset: xarray.Dataset, hash_type: type[hashlib._Hash] | Any
         The dataset to generate a cache key from
     hash : hash class
         The kind of hash to use.
-        Defaults to `hashlib.sha1`, which is secure enough and fast enough for most purposes.
+        Defaults to `hashlib.blake2b`, which is secure enough and fast enough for most purposes.
         The hash algorithm does not need to be cryptographically secure,
         so faster algorithms such as `xxhash` can be swapped in if desired.
 
