839f458f3395f3925b379255ae7769a8f6501d6e
David Secretan-Hallett <david.secretan-hallett@csiro.au>
2024-09-17 11:31:47 +1000

    Updated hash_attributes to use marshal instead of pickle for serialisation. Updated make_cache_key to accept a hash instance and to hash the convention name, module path and emsarray version.

--- src/emsarray/operations/cache.py
+++ src/emsarray/operations/cache.py
@@ -1,34 +1,40 @@
 """
-Operations for making a triangular mesh out of the polygons of a dataset.
+Operations for making caching keys for given datasets and attribute dicts.
 """
-from __future__ import annotations
-
 import hashlib
-import inspect
-import pickle
-from typing import Any
+import marshal
 
+import numpy
 import xarray
 
+import emsarray
 
-def hash_attributes(hash: hashlib._Hash, attribute_dict: xarray.Variable) -> None:
+
+def hash_attributes(hash: "hashlib._Hash", attributes: dict) -> None:
     """
-    Update the provided hash with with a pickle serialised byte representation of the given attribute dictionary.
+    Updates the provided hash with with a marshal serialised byte representation of the given attribute dictionary.
 
     Parameters
     ----------
     hash : hashlib-style hash instance
-        The hash instance to update with geometry data.
+        The hash instance to update with the given attribute dict.
         This must follow the interface defined in :mod:`hashlib`.
-    attribute_dict: xarray.Variable dictionary
-        Expects a pickleable xarray.Variable dictionary
+    attributes: dict
+        Expects a marshal compatible dictionary
     """
-    # Specifying pickle protocol version to prevent hash changes when the default is updated
-    attribute_dict_bytes = pickle.dumps(attribute_dict, protocol=5)
-    hash.update(attribute_dict_bytes)
+    # Prepend the marshal encoding version
+    marshal_version = 4
+    hash.update(numpy.int32(marshal_version).tobytes('C'))
+    # Specify marshal encoding version when serialising
+    attribute_dict_marshal_bytes = marshal.dumps(attributes, marshal_version)
+    # Prepend the number of attributes
+    hash.update(numpy.int32(len(attributes)).tobytes('C'))
+    # Prepend the size of the pickled attributes
+    hash.update(numpy.int32(len(attribute_dict_marshal_bytes)).tobytes())
+    hash.update(attribute_dict_marshal_bytes)
 
 
-def make_cache_key(dataset: xarray.Dataset, hash_type: type[hashlib._Hash] | Any = hashlib.blake2b) -> str:
+def make_cache_key(dataset: xarray.Dataset, hash: "hashlib._Hash | None" = None) -> str:
     """
     Generate a key suitable for caching data derived from the geometry of a dataset.
 
@@ -36,8 +42,8 @@ def make_cache_key(dataset: xarray.Dataset, hash_type: type[hashlib._Hash] | Any
     ----------
     dataset : xarray.Dataset
         The dataset to generate a cache key from
-    hash : hash class
-        The kind of hash to use.
+    hash : hash instance
+        An instance of a hashlib hash class
         Defaults to `hashlib.blake2b`, which is secure enough and fast enough for most purposes.
         The hash algorithm does not need to be cryptographically secure,
         so faster algorithms such as `xxhash` can be swapped in if desired.
@@ -52,21 +58,17 @@ def make_cache_key(dataset: xarray.Dataset, hash_type: type[hashlib._Hash] | Any
     -----
     The cache key will depend on the Convention class,
     the emsarray version, and a hash of the geometry of the dataset.
-    The specific structure of the cache key may change between emsarray versions
+    The specific structure of the cache key may change between emsarray and python versions,
     and should not be relied upon.
     """
-    hash_sig = inspect.signature(hash_type.__init__)
+    if hash is None:
+        hash = hashlib.blake2b(digest_size=32)
 
-    if ('digest_size' in hash_sig.parameters):
-        m = hash_type(digest_size=32)
-    else:
-        m = hash_type()
+    dataset.ems.hash_geometry(hash)
 
-    dataset.ems.hash_geometry(m)
+    # Hash convention name, convention module path and emsarray version
+    hash.update(dataset.ems.__class__.__module__.encode('utf-8'))
+    hash.update(dataset.ems.__class__.__name__.encode('utf-8'))
+    hash.update(emsarray.__version__.encode('utf-8'))
 
-    geometry_names = dataset.ems.get_all_geometry_names()
-
-    for geometry_name in geometry_names:
-        hash_attributes(m, dataset.variables[geometry_name])
-
-    return m.hexdigest()
+    return hash.hexdigest()
