d7bb46cb3964d0caa88d389f27da8b00aee983c7
Tim Heap <tim.heap@csiro.au>
2025-01-22 13:54:55 +1100

    Restore commented out section

--- scripts/update_pinned_dependencies.sh
+++ scripts/update_pinned_dependencies.sh
@@ -32,32 +32,32 @@ conda run \
 	python3 ./scripts/min_deps_check.py "$requirements_file"
 conda env remove --yes --prefix="${conda_prefix}"
 
-# for version in "${PYTHON_VERSIONS[@]}" ; do
-# 	requirements_file="./continuous-integration/requirements-${version}.txt"
-# 	echo "Updating $requirements_file"
-# 
-# 	conda_prefix="${conda_venv_root}/py${version}"
-# 	conda create \
-# 		--yes --quiet \
-# 		--prefix="${conda_prefix}" \
-# 		--no-default-packages
-# 	conda install \
-# 		--yes \
-# 		--prefix="${conda_prefix}" \
-# 		--channel conda-forge \
-# 		"python=${version}" \
-# 		pip-tools
-# 	conda run \
-# 		--prefix="${conda_prefix}" \
-# 		pip-compile \
-# 			--upgrade \
-# 			--extra="testing" \
-# 			--output-file="${requirements_file}" \
-# 			--unsafe-package emsarray \
-# 			--no-allow-unsafe \
-# 			pyproject.toml
-# 	conda env remove --yes --prefix="${conda_prefix}"
-# done
+for version in "${PYTHON_VERSIONS[@]}" ; do
+	requirements_file="./continuous-integration/requirements-${version}.txt"
+	echo "Updating $requirements_file"
+
+	conda_prefix="${conda_venv_root}/py${version}"
+	conda create \
+		--yes --quiet \
+		--prefix="${conda_prefix}" \
+		--no-default-packages
+	conda install \
+		--yes \
+		--prefix="${conda_prefix}" \
+		--channel conda-forge \
+		"python=${version}" \
+		pip-tools
+	conda run \
+		--prefix="${conda_prefix}" \
+		pip-compile \
+			--upgrade \
+			--extra="testing" \
+			--output-file="${requirements_file}" \
+			--unsafe-package emsarray \
+			--no-allow-unsafe \
+			pyproject.toml
+	conda env remove --yes --prefix="${conda_prefix}"
+done
 
 
 echo rm -rf "$conda_venv_root"
