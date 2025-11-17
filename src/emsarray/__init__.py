6d40c3a6085a00ecf3ec23070342d3a98b4893f5
Tim Heap <tim.heap@csiro.au>
2025-11-17 12:07:41 +1100

    Drop old deprecated code

--- src/emsarray/__init__.py
+++ src/emsarray/__init__.py
@@ -2,8 +2,6 @@
 emsarray - xarray utilities for various non-CF grid data conventions
 """
 import importlib.metadata
-import warnings
-from typing import Any
 
 from . import tutorial
 from .accessors import ems_accessor
@@ -16,20 +14,3 @@ __all__ = [
     "ems_accessor",
     "Convention", "get_dataset_convention", "open_dataset",
 ]
-
-
-def __getattr__(name: str) -> Any:
-    old_new = {
-        'Format': ('Convention', 'emsarray.formats.Format'),
-        'get_file_format': ('get_dataset_convention', 'emsarray.formats.get_file_format'),
-    }
-    if name in old_new:
-        new, old_path = old_new[name]
-        warnings.warn(
-            f"emsarray.{name} has been renamed to emsarray.{new}",
-            category=DeprecationWarning,
-            stacklevel=2)
-        old_module, old_name = old_path.rsplit('.', 1)
-        return getattr(importlib.import_module(old_module), old_name)
-
-    raise AttributeError(f"{name} is not defined")
